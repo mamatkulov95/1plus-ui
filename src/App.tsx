d5fd2285e10f35b1b236c18a2d531961a0ddecf4
Ma'rufjon <m.mamatkulov95@gmail.com>
2023-09-25 11:44:31 +0500

    [Hero] -Developed hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from "./components/Header/Header";
-import "bootstrap/dist/css/bootstrap.min.css";
+import Hero from "./components/Hero/Hero";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Hero />
     </div>
   );
 }
